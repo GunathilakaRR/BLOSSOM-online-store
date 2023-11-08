e51081404a35235444f9903b0b9f013de302e017
GunathilakaRR <rusirugunathilaka118@gmail.com>
2023-11-08 11:41:46 +0530

    Add user and admin dashboards and paths

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,39 @@
-<x-app-layout>
-    
-</x-app-layout>
+{{-- <x-app-layout>
+
+</x-app-layout> --}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    {{-- css files --}}
+    @include('admin.css')
+
+</head>
+
+<body>
+
+
+
+    <!-- partial -->
+
+    {{-- sidebar panel --}}
+    @include('admin.sidebar')
+
+    {{-- navigationbar --}}
+    @include('admin.navbar')
+
+
+
+    <!-- partial -->
+
+    @include('admin.body')
+    <!-- partial -->
+
+    @include('admin.script')
+
+</body>
+
+</html>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -184,39 +184,7 @@
 
 
     <!-- feature -->
-    <section id="features">
-        <div class="row">
-            <div class="feature-box col-lg-4">
-                <img src="./images/item1.jpg" alt="" width="300px">
-                <p>CASUAL WEAR</p>
-            </div>
-
-            <div class="feature-box col-lg-4">
-                <img src="./images/item2.jpg" alt="" width="300px">
-                <p>OFFICE WEAR</p>
-            </div>
-
-            <div class="feature-box col-lg-4">
-                <img src="./images/item3.jpg" alt="" width="300px">
-                <p>MINI WEAR</p>
-            </div>
-
-            <div class="feature-box col-lg-4">
-                <img src="./images/item4.jpg" alt="" width="300px">
-                <p> OFFICE WEAR</p>
-            </div>
-
-            <div class="feature-box col-lg-4">
-                <img src="./images/item5.jpg" alt="" width="300px">
-                <p>PARTY WEAR</p>
-            </div>
-
-            <div class="feature-box col-lg-4">
-                <img src="./images/item6.jpg" alt="" width="300px">
-                <p>CASUAL WEAR</p>
-            </div>
-        </div>
-    </section>
+    @include('user.products')
 
 
     <!-- Testimonials -->
