c1b4e01bb9dacc7b080675bdc6ab7444bff43e25
GunathilakaRR <rusirugunathilaka118@gmail.com>
2023-11-08 14:59:40 +0530

    Add admin dashboard & upload data to the database

---
+++ resources/views/admin/product.blade.php
@@ -0,0 +1,112 @@
+{{-- <x-app-layout>
+
+</x-app-layout> --}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    {{-- css files --}}
+    @include('admin.css')
+
+    <style>
+        .title {
+            color: white;
+            padding-top: 25px;
+        }
+        label{
+            display: inline-block;
+            width: 200px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+
+
+    <!-- partial -->
+
+    {{-- sidebar panel --}}
+    @include('admin.sidebar')
+
+    {{-- navigationbar --}}
+    @include('admin.navbar')
+
+
+
+    <!-- partial -->
+
+    <div class="container-fluid page-body-wrapper">
+        <div class="container" align="center">
+            <h1 class="title">Add Product</h1>
+
+            @if (session()->has('message'))
+
+            <div class="alert alert-success">
+
+            <button type="button" class="close" data-dismiss='alert'>x</button>
+            {{ session()->get('message') }}
+
+        </div>
+            @endif
+
+            <form action="{{ url('uploadproduct') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div style="padding: 20px">
+
+                    <label>Product Title</label>
+                    <input type="text" name="title" placeholder="Product title">
+                </div>
+
+                <div style="padding: 20px">
+
+                    <label>Product Price</label>
+                    <input type="number" name="price" placeholder="Product price">
+                </div>
+
+                <div style="padding: 20px">
+
+                    <label>Product Description</label>
+                    <input type="text" name="desc" placeholder="Product description">
+                </div>
+
+                <div style="padding: 20px">
+
+                    <label>Product Quantity</label>
+                    <input type="text" name="quantity" placeholder="Product quantity">
+                </div>
+
+                <div style="padding: 20px">
+
+                    <label>Product Iamge</label>
+                    <input type="file" name="file">
+                </div>
+
+                <div style="padding: 20px">
+                    <input type="submit">
+                </div>
+
+
+            </form>
+
+
+        </div>
+    </div>
+
+
+    
+    <!-- partial -->
+
+
+
+
+
+    @include('admin.script')
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +33,7 @@ Route::middleware([
 Route::get('/redirect', [HomeController::class, 'redirect']);
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/product', [AdminController::class, 'product']);
+
+Route::post('/uploadproduct', [AdminController::class, 'uploadproduct']);
